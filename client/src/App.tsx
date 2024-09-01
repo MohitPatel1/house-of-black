afca2a1f6744b4cbc9e13d17cf1e46259677ec60
mohitpatel1 <mohit.patel1966@gmail.com>
2024-09-01 16:49:14 +0530

    toast added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { 
   Stack, 
